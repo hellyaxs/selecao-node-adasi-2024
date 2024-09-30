4307be3a55e09170793eba1336ff51be2643a7db
Elias <eliasvitor.dev@gmail.com>
2024-09-30 13:04:35 -0300

    filtro para exeções

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,10 @@ import { CursosModule } from './infra/resources/cursos/cursos.module';
 import { AtividadesModule } from './infra/resources/atividades/atividades.module';
 import { EstudantesModule } from './infra/resources/estudantes/estudantes.module';
 import { TarefasModule } from './infra/resources/tarefas/tarefas.module';
+import { AllExceptionsFilter } from './allexceptionsfilter';
 
 @Module({
   imports: [CursosModule, AtividadesModule, EstudantesModule, TarefasModule],
+  providers: [AllExceptionsFilter],
 })
 export class AppModule {}
